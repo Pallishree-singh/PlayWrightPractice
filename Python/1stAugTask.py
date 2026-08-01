dc4d62084d87cd83e111887578801cb6990d82ae
Pallishree Singh <pallishree.singh@wisetechglobal.com>
2026-08-01 21:01:30 +0530

    Add Python 1st Aug-tasks and labs

---
+++ Python/1stAugTask.py
@@ -0,0 +1,16 @@
+num1=input("enter 1st num:  ")
+num2=input("enter 2nd num:  ")
+num3=input("enter 3rd num:  ")
+
+sum=int(num1)+int(num2)+int(num3)
+print("sum of 3 numbers is:",sum)
+
+substraction=int(num1)-int(num2)-int(num3)
+print("substraction of 3 numbers is:",substraction)
+
+mul=int(num1)*int(num2)*int(num3)
+print("multiplication of 3 numbers is:",mul)
+
+div=int(num1)/int(num2)/int(num3)
+print("division of 3 numbers is:",div)
+
---
+++ Python/1stAugTask2.py
@@ -0,0 +1,8 @@
+num1=int(input("enter 1st number:  "))
+num2=int(input("enter 2nd number:  "))
+
+Quotient=num1/num2
+print("Quotient of 2 numbers is:",Quotient) 
+
+Remainder=num1%num2
+print("Remainder of 2 numbers is:",Remainder)
